4abf4ab5107d503d7c24648205aa7fb34e84553f
ALESHA <a.sajid@studenti.unibg.it>
2026-09-24 17:11:04 +0200

    error resolved

---
+++ codice/App.java
@@ -0,0 +1,12 @@
+package se_project.Calculator;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	
+        System.out.println("La somma di 3 + 5 e");
+        System.out.println(Calculator.somma(3, 5));
+    }
+}
---
+++ codice/Calculator.java
@@ -0,0 +1,11 @@
+package se_project.Calculator;
+
+
+
+public class Calculator {
+
+	static int somma( int x , int y ) {
+		return x+y;
+	}
+
+}
